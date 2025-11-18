50a7fd1e4bfb4463a9285c72f5c2e23e26725011
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-11-18 08:33:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClass();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClass() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
+}
+
